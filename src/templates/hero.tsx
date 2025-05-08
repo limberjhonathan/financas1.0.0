cf08cacd602a8ff54bee1ea8023734aef8b62382
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-07 23:36:50 -0300

    mudança de pasta

---
+++ src/templates/hero.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ButtonLink } from "@/components/button-link";
+
+export function Hero() {
+  return (
+    <main>
+      <header>
+        <nav className="flex justify-between items-center px-[3em] py-[1em] shadow-lg">
+          <Link href="/" className="w-[100px] h-[60px]">
+            <Image className="h-full w-full object-contain" src="/logo/logo.png" alt="Logo" width={100} height={100} />
+          </Link>
+          <h1 className="uppercase text-[1.4em] font-cascadia tracking-[10px]">altus</h1>
+          <div>
+            <ButtonLink text="login" />
+            <ButtonLink text="cadastrar" />
+          </div>
+        </nav>
+      </header>
+    </main>
+  );
+}

f60c12d78974730cf4ca030ccd42285e8a439b21
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-12 23:49:07 -0300

    identação

--- src/templates/hero.tsx
+++ src/templates/hero.tsx
@@ -4,19 +4,17 @@ import { ButtonLink } from "@/components/button-link";
 
 export function Hero() {
   return (
-    <main>
-      <header>
-        <nav className="flex justify-between items-center px-[3em] py-[1em] shadow-lg">
-          <Link href="/" className="w-[100px] h-[60px]">
-            <Image className="h-full w-full object-contain" src="/logo/logo.png" alt="Logo" width={100} height={100} />
-          </Link>
-          <h1 className="uppercase text-[1.4em] font-cascadia tracking-[10px]">altus</h1>
-          <div>
-            <ButtonLink text="login" />
-            <ButtonLink text="cadastrar" />
-          </div>
-        </nav>
-      </header>
-    </main>
+    <header>
+      <nav className="flex justify-between items-center px-[3em] py-[1em] shadow-lg">
+        <Link href="/" className="w-[100px] h-[60px]">
+          <Image className="h-full w-full object-contain" src="/logo/logo.png" alt="Logo" width={100} height={100} priority/>
+        </Link>
+        <h1 className="uppercase text-[1.4em] font-cascadia tracking-[10px]">altus</h1>
+        <div>
+          <ButtonLink text="login" />
+          <ButtonLink text="cadastrar" />
+        </div>
+      </nav>
+    </header>
   );
 }

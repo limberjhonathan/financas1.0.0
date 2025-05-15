3cf2ced8ab501d6f25d458899b1a5547da6eb56b
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-14 23:33:29 -0300

    style component

--- src/components/button-link.tsx
+++ src/components/button-link.tsx
@@ -6,8 +6,9 @@ export interface Titulo {
 
 export function ButtonLink(props: Titulo) {
     return (
-        <Link href={`/${props.text}`} className="text-black-300 border-[1.5px] border-gray-400 px-4 py-2 hover:bg-background-button hover:text-white hover:border-transparent mr-3 rounded-[5px] transition-all duration-[400ms] ease-in">
+        <Link href={`/${props.text}`} className="text-black-300 border-[1.5px] border-gray-400 px-4 py-2 hover:bg-[var(--cor-primaria)] hover:text-white hover:border-transparent mr-3 rounded-[5px] transition-all duration-[400ms] ease-in">
             {props.text}
         </Link>
+
     )
 }

c1a5965c33ba5be1f447bac8a5d502fc10a03da2
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-15 23:28:07 -0300

    ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import { Hero } from "../templates/hero";
+import { Hero } from "@/templates/hero";
+import { generatePageMetadata } from "@/utils/page-title";
+
+export const metadata = generatePageMetadata("Home", "Home page")
 
 export default function Home() {
   return (

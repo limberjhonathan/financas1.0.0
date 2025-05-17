aae0a34bb6b9c6e202fe42a97b4e628530cbd12f
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-16 23:17:35 -0300

    layout form

---
+++ src/templates/layoutBack-form.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface LayoutBackFormProps {
+  children: ReactNode;
+}
+
+export function LayoutBackForm({ children }: LayoutBackFormProps) { 
+    return(
+        <div className="bg-[var(--background)] min-h-[100dvh] text-white flex flex-col items-center justify-between p-2">
+            <div className="flex flex-col items-center gap-3 2xl:gap-8 2xl:mt-20 2xl:text-[1em] text-[.8em] ">
+                <div>
+                    <Image src="/background/bg-altus.png" alt="Background Altus" width={150} height={150} />
+                </div>
+                {children}
+            </div>
+            <span className="font-bold text-gray-600 transform lg:scale-75">Desenvolvido por <Link href={'https://www.linkedin.com/in/limber-jhonathan/'} className="underline text-emerald-800">Limber Jhonathan</Link></span>
+        </div>
+    )
+}

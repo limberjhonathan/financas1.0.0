68b9f56045b4d4ec20e6d87fe3e3059a456dfa52
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-16 23:18:08 -0300

    title for form

---
+++ src/components/form-title.tsx
@@ -0,0 +1,5 @@
+export function TitleForm({ children }: { children: React.ReactNode }) {
+    return (
+        <h1 className=" text-center 2xl:text-3xl text-2xl mb-8 font-bold">{children}</h1>
+    )
+}  

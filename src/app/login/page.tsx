2947984fed53166de1ec9cd4f51756bb82ce489d
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-16 23:20:43 -0300

    adjustments

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,26 @@
+import { Input, Button } from "@/components/form-components"
+import { FormLayout } from "@/components/form-layout"
+import { TitleForm } from "@/components/form-title"
+import { LayoutBackForm } from "@/templates/layoutBack-form"
+import { generatePageMetadata } from "@/utils/page-title"
+import Link from "next/link"
+
+export const metadata = generatePageMetadata("Login", "Login page")
+
 export default function Login() {
-    return(
-        <h1>Login</h1>
+
+    return (
+        <LayoutBackForm>
+            <div>
+                <TitleForm>Login</TitleForm>
+                <FormLayout>
+                    <Input text="E-mail ou usuário" />
+                    <Input text="Senha" />
+                    <Button text="acessar" />
+                </FormLayout>
+                <p className="text-center p-2">Ainda não possui conta? <Link href={'/cadastrar'} className="text-[var(--form-bg)] font-bold cursor-pointer underline">Cadastre-se</Link></p>
+            </div>
+        </LayoutBackForm>
+
     )
 }

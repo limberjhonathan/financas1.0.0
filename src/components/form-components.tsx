e1a815043e7b1343555c496ea17d425d7c596803
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-20 23:31:01 -0300

    icone vizualizar

--- src/components/form-components.tsx
+++ src/components/form-components.tsx
@@ -1,36 +1,60 @@
-export interface Text {
+"use client"
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
+import { useState } from 'react'
+
+export interface InputText {
+    text?: string;
+    type?: string;
+}
+export interface ButtonText {
     text?: string;
 }
 
-export function Input(props: Text) {
+export function Input(props: InputText) {
+    const [showPassword, setShowPassword] = useState(false)
+    const isPassword = props.type === 'password'
+
     return (
-        <input
-            type="text"
-            placeholder={props.text}
-            className="
-                w-full
-                2xl:h-13
-                2xl:px-3
-                2xl:py-4
-                h-9
-                px-3
-                text-[1em]
-                xl:text-[.9em]
-                border-2 border-[var(--form-bg)]
-                rounded-md
-                font-sans
-                font-bold
-                leading-[3rem]
-                focus:outline-none
-                focus:ring-2 focus:ring-[var(--form-bg)]
-                focus:shadow-[0_0_20px_var(--cor-primaria)]
-                transition-shadow duration-700"
-        />
+        <div className='relative w-full'>
+            <input
+                type={isPassword && !showPassword ? 'password' : 'text'}
+                placeholder={props.text}
+                className="
+            w-full
+            2xl:h-13
+            2xl:pl-3
+            2xl:pr-9
+            2xl:py-4
+            h-12
+            pl-3
+            pr-9
+            text-[1em]
+            xl:text-[.9em]
+            border-2 border-[var(--form-bg)]
+            rounded-md
+            font-sans
+            font-bold
+            leading-[3rem]
+            focus:outline-none
+            focus:ring-2 focus:ring-[var(--form-bg)]
+            focus:shadow-[0_0_20px_var(--cor-primaria)]
+            transition-shadow duration-700"
+            />
+            {isPassword && (
+                <div
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-600 2xl:text-[1.3em] text-[1.5em]"
+                >
+                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                </div>
+            )}
+
+        </div>
     )
 }
 
-export function Button(props: Text) {
+export function Button(props: ButtonText) {
     return (
-        <button className="w-full h-9 2xl:h-13 bg-[var(--form-bg)] rounded-md text-[var(--background))] font-sans font-bold first-letter:uppercase hover:bg-[#1f605a] transition-bg duration-800">{props.text}</button>
+        <button className="w-full h-12 2xl:h-13 bg-[var(--form-bg)] rounded-md text-[var(--background))] font-sans font-bold first-letter:uppercase hover:bg-[#1f605a] transition-bg duration-800">{props.text}</button>
     )
 }

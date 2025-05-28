eede3f4d7366fb353e9b3ac566c20378a468b10b
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-27 23:43:37 -0300

    notification error

--- src/components/notification.tsx
+++ src/components/notification.tsx
@@ -1,10 +1,10 @@
-export default function Notification({ message, success }: { message: string | undefined, success: boolean | undefined }) {
+export default function Notification({ message, error }: { message: string | undefined, error: boolean | undefined }) {
   return (
     <div
       className={`
         fixed left-1/2 transform -translate-x-1/2 top-3 z-50
         px-6 py-3 rounded-xl shadow-lg text-white text-sm font-semibold
-        ${success ? "bg-green-500" : "bg-red-500"}
+        ${error ? "bg-green-500" : "bg-red-500"}
         transition-opacity duration-700 ease-out animate-mymove
       `}
     >

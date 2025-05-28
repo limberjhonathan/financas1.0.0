e54b430adec33a64b2ec7715f172a3dd94301578
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-27 23:44:55 -0300

    input config error

--- src/components/form-components.tsx
+++ src/components/form-components.tsx
@@ -5,9 +5,11 @@ import { useState } from 'react'
 export interface InputText {
     text?: string;
     type?: string;
+    name?: string;
+    error?: boolean;
 }
 export interface ButtonText {
-    text?: string;
+  text?: string;
 }
 
 export function Input(props: InputText) {
@@ -18,27 +20,31 @@ export function Input(props: InputText) {
         <div className='relative w-full'>
             <input
                 type={isPassword && !showPassword ? 'password' : 'text'}
+                name={props.name}
                 placeholder={props.text}
-                className="
-            w-full
-            2xl:h-13
-            2xl:pl-3
-            2xl:pr-9
-            2xl:py-4
-            h-12
-            pl-3
-            pr-9
-            text-[1em]
-            xl:text-[.9em]
-            border-2 border-[var(--form-bg)]
-            rounded-md
-            font-sans
-            font-bold
-            leading-[3rem]
-            focus:outline-none
-            focus:ring-2 focus:ring-[var(--form-bg)]
-            focus:shadow-[0_0_20px_var(--cor-primaria)]
-            transition-shadow duration-700"
+                autoComplete='off'
+                className={`
+                    w-full
+                    2xl:h-13
+                    2xl:pl-3
+                    2xl:pr-9
+                    2xl:py-4
+                    h-12
+                    pl-3
+                    pr-9
+                    text-[1em]
+                    xl:text-[.9em]
+                    border-2
+                    rounded-md
+                    font-sans
+                    font-bold
+                    leading-[3rem]
+                    focus:outline-none
+                    transition-shadow duration-700
+                    ${props.error
+                        ? 'border-red-500 focus:ring-2 focus:ring-red-500 focus:border-red-500 focus:shadow-[0_0_20px_var(--cor-error)]'
+                        : 'border-[var(--form-bg)] focus:ring-2 focus:ring-[var(--form-bg)] focus:shadow-[0_0_20px_var(--cor-primaria)]'}
+                `}
             />
             {isPassword && (
                 <div
@@ -48,13 +54,14 @@ export function Input(props: InputText) {
                     {showPassword ? <FaEye /> : <FaEyeSlash />}
                 </div>
             )}
-
         </div>
     )
 }
 
 export function Button(props: ButtonText) {
     return (
-        <button className="w-full h-12 2xl:h-13 bg-[var(--form-bg)] rounded-md text-[var(--background))] font-sans font-bold first-letter:uppercase hover:bg-[#1f605a] transition-bg duration-800">{props.text}</button>
+        <button className="w-full h-12 2xl:h-13 bg-[var(--form-bg)] rounded-md text-[var(--background))] font-sans font-bold first-letter:uppercase hover:bg-[#1f605a] transition-bg duration-800">
+            {props.text}
+        </button>
     )
 }

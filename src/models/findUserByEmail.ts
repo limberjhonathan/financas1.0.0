4c3bf71e2e6b942026015eb13e52df5d327dc83e
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-29 23:32:11 -0300

    config login action

---
+++ src/models/findUserByEmail.ts
@@ -0,0 +1,51 @@
+import db from "../lib/db";
+import { compareSync } from "bcryptjs";
+
+
+export async function findUserByEmail(email: string) {
+  return await db.user.findUnique({ where: { email } });
+}
+
+export async function createUser(data: {
+  email: string;
+  password: string;
+  username: string;
+}) {
+  return await db.user.create({
+    data: {
+      email: data.email,
+      password: data.password,
+      username: data.username,
+      createdAt: new Date(),
+    },
+  });
+}
+
+type User = {
+  email: string;
+  password?: string;
+  name?: string;
+}
+
+export async function findUserByCredentials(
+  email: string,
+  password: string
+): Promise<User | null> {
+  const user = await db.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    return null;
+  }
+
+  const passwordMatch = compareSync(password, user.password);
+
+  if(passwordMatch) {
+    return {email: user.email, name: user.username};
+  }
+
+  return null;
+} 

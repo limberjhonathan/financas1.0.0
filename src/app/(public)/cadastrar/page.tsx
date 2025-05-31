a1d36922f29ed1d556e48dd2ff0de6d0b9486be9
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-31 19:32:43 -0300

    page register

--- src/app/(public)/cadastrar/page.tsx
+++ src/app/(public)/cadastrar/page.tsx
@@ -1,24 +1,17 @@
-import { Button, Input } from "@/components/form-components";
-// import { FormLayout } from "@/components/form-layout";
-import Form from 'next/form'
 
-import { TitleForm } from "@/components/form-title";
-import { LayoutBackForm } from "@/templates/layoutBack-form";
 import Link from "next/link";
-import registerAction from "./registerAction";
 
-export default function Cadastrar() {
+import RegisterForm from "./register-form";
+import { LayoutBackForm } from "@/src/templates/layoutBack-form";
+import { TitleForm } from "@/src/components/form-title";
+
+export default async function Cadastrar() {
+
     return (
         <LayoutBackForm>
             <div>
                 <TitleForm>Cadastre-se</TitleForm>
-                <Form action={registerAction} className="flex flex-col gap-3 mt-4 w-[350px] 2xl:gap-5 2xl:w-[400px]">
-                    <Input text="Nome do usuário" name="name"/>
-                    <Input text="Email" type="text" name="email"/>
-                    <Input text="Senha" type="password" name="password"/>
-                    <Input text="Digite novamente a senha" type="password" name="repeatPassword"/>
-                    <Button text="Cadastrar" />
-                </Form>
+                <RegisterForm />
                 <p className="text-center p-2">Ja possui conta? <Link href={'/login'} className="text-[var(--form-bg)] font-bold cursor-pointer underline">Faça Login</Link></p>
             </div>
         </LayoutBackForm>

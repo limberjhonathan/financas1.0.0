dec1cf7678d38d24ad6ba31ab12701d097c6ff2c
Limber Jhonathan <limberjhonathan@gmail.com>
2025-05-31 19:23:04 -0300

    page config dash

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,23 +1,26 @@
-import { Input, Button } from "@/components/form-components"
-import { FormLayout } from "@/components/form-layout"
-import { TitleForm } from "@/components/form-title"
-import { LayoutBackForm } from "@/templates/layoutBack-form"
-import { generatePageMetadata } from "@/utils/page-title"
+
+import { TitleForm } from "@/src/components/form-title"
+import { LayoutBackForm } from "@/src/templates/layoutBack-form"
+import { generatePageMetadata } from "@/src/utils/page-title"
 import Link from "next/link"
+import LoginForm from "./login-form"
+// import { auth } from "@/auth"
+// import { redirect } from "next/navigation"
 
 export const metadata = generatePageMetadata("Login", "Login page")
 
-export default function Login() {
+export default async function Login() {
+    // const session = await auth()
+
+    // if (session) {
+    //     redirect('/dashboard')
+    // }
 
     return (
         <LayoutBackForm>
             <div>
                 <TitleForm>Login</TitleForm>
-                <FormLayout>
-                    <Input text="E-mail ou usuário" type="text"/>
-                    <Input text="Senha" type="password"/>
-                    <Button text="acessar" />
-                </FormLayout>
+                <LoginForm />
                 <p className="text-center p-2">Ainda não possui conta? <Link href={'/cadastrar'} className="text-[var(--form-bg)] font-bold cursor-pointer underline">Cadastre-se</Link></p>
             </div>
         </LayoutBackForm>

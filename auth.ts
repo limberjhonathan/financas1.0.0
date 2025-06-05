3acf1373c25ee4874477e964fbc0bed6b675d4a0
Limber Jhonathan <limberjhonathan@gmail.com>
2025-06-04 23:20:07 -0300

    user confirmed

--- auth.ts
+++ auth.ts
@@ -1,6 +1,10 @@
-import NextAuth from "next-auth"
+import NextAuth, { CredentialsSignin } from "next-auth"
 import Credentials from "next-auth/providers/credentials";
 import { findUserByCredentials } from "./src/models/findUserByEmail";
+
+class UserNotConfirmedError extends CredentialsSignin {
+  code = "UserNotConfirmed"
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -13,6 +17,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         // console.log(credentials);
 
         const user = await findUserByCredentials(credentials.email as string, credentials.password as string);
+        if (!user) return null;
+
+        if(!user?.isConfirmed){
+          throw new UserNotConfirmedError()
+        }
         return user
       },
     })

e222c8de126a98dd6bee832915e44ed4dee2624f
Limber Jhonathan <limberjhonathan@gmail.com>
2025-06-06 23:36:05 -0300

    cookie

--- src/app/(public)/login/loginAction.ts
+++ src/app/(public)/login/loginAction.ts
@@ -1,15 +1,17 @@
 "use server";
 
 import { signIn } from "@/auth";
+import { generateEmailToken } from "@/src/utils/generateEmailToken";
+// import { sendVerificationEmail } from "@/src/lib/email/sendVerificationEmail";
+// import { generateCode } from "@/src/utils/generateCode";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default async function loginAction(_prevState: any, formData: FormData) { 
+export default async function loginAction(_prevState: any, formData: FormData) {
     try {
         const email = formData.get("email") as string;
         const password = formData.get("password") as string;
-
-        console.log("Login attempt with email:", email);
-        console.log("Login attempt with password:", password)
 
         if (!email || !password) {
             return { success: false, message: "Email e senha são obrigatórios" };
@@ -21,16 +23,36 @@ export default async function loginAction(_prevState: any, formData: FormData) {
             redirect: true,
             redirectTo: "/dashboard",
         });
-    } 
+    }
     catch (e: any) {
-        if(isRedirectError(e)) {
+        const email = formData.get("email") as string
+
+        // const code = generateCode()
+
+        if (isRedirectError(e)) {
             throw e;
+        }
+        if (e.code === "UserNotConfirmed") {
+            const cookieStore = await cookies();
+            cookieStore.set({
+                name: "email_not_confirmed",
+                value: generateEmailToken(email),
+                path: "/",
+                maxAge: 60 * 3,
+                httpOnly: true,
+                sameSite: "lax",
+                secure: process.env.NODE_ENV === "production",
+            });
+            // Enviar o email
+            // await sendVerificationEmail(email, code);
+
+            redirect("/verificar");
         }
 
         if (e?.type === "CredentialsSignin") {
             return { success: false, message: "Senha ou email inválidos" };
         }
-        console.error("Erro ao fazer login:", e);
+
         return { success: false, message: "Dados de Login inválidos" };
     }
 }

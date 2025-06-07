9bfff2bb064c77e8aa9da5924d5add8eebee64f5
Limber Jhonathan <limberjhonathan@gmail.com>
2025-06-06 23:34:48 -0300

    modify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { generatePageMetadata } from "../utils/page-title";
 export const metadata = generatePageMetadata("Home", "Home page")
 
 export default function Home() {
+
   return (
     <main>
       <Hero />

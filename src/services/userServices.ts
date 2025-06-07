fbc6150d8f02492a5719f4652e21d2f2a3ac22ec
Limber Jhonathan <limberjhonathan@gmail.com>
2025-06-06 23:28:30 -0300

    add use Service

---
+++ src/services/userServices.ts
@@ -0,0 +1,67 @@
+import { compareSync } from "bcryptjs";
+import db from "../lib/db";
+import { generateCode } from "../utils/generateCode";
+import { updateCodeUser } from "../models/useModel";
+
+type User = {
+    email: string;
+    password?: string;
+    name?: string;
+    isConfirmed: boolean;
+}
+
+export async function createExpiringConfirmationCode(code: string, userId: number) {
+    await db.confirmationCode.create({
+        data: {
+            code,
+            user: {
+                connect: { id: userId }
+            },
+            expiresAt: new Date(Date.now() + 3 * 60 * 1000),
+        },
+    });
+}
+
+export async function findUserByCredentials(
+  email: string,
+  password: string
+): Promise<User | null> {
+  const user = await db.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+  if (!user) {
+    return null;
+  }
+
+  const passwordMatch = compareSync(password, user.password);
+
+  if (passwordMatch) {
+    return { email: user.email, name: user.username, isConfirmed: user.isConfirmed };
+  }
+
+  return null;
+}
+
+export async function checkDateNow(
+  id: number,
+  createdAt?: Date,
+  expiresAt?: Date
+){
+  if (!createdAt || !expiresAt) {
+    return;
+  }
+
+  const now = new Date();
+  if (expiresAt <= now) {
+    const newCode = generateCode();
+    return await updateCodeUser(newCode, id);
+    
+  }
+}
+
+// export default async function createNewCode(email: string){
+//   const user = await findUserByEmail(email)
+
+// }

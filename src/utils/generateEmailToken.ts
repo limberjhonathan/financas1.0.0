c0172c1c9dd1d00c6bdc795c831d73d36176b5de
Limber Jhonathan <limberjhonathan@gmail.com>
2025-06-11 23:42:45 -0300

    generate token email

--- src/utils/generateEmailToken.ts
+++ src/utils/generateEmailToken.ts
@@ -3,5 +3,5 @@ import jwt from "jsonwebtoken";
 const secret = process.env.JWT_SECRET || "chave_super_secreta";
 
 export function generateEmailToken(email: string) {
-  return jwt.sign({ email }, secret, { expiresIn: "5m" });
+  return jwt.sign({ email }, secret, { expiresIn: "1m" });
 }

7749250bd68dbf7795bfbca189a1d4ac121bccba
Limber Jhonathan <limberjhonathan@gmail.com>
2025-06-12 23:53:07 -0300

    config buttons

--- src/components/buttonAction.tsx
+++ src/components/buttonAction.tsx
@@ -3,16 +3,19 @@ import { buttonClass } from "../utils/style/buttonStyle";
 interface ButtonActionProps {
     text: string;
     disabled?: boolean;
-    onClick: () => void;
+    onClick?: () => void;
     baseClass?: string;
+    type?: "button" | "submit" | "reset";
 }
 
 export default function ButtonAction(props: ButtonActionProps) {
+    const disabledStyle = props.disabled ? "opacity-50 cursor-not-allowed" : ""
     return (
         <button
+            type={props.type}
             disabled={props.disabled}
             onClick={props.onClick}
-            className={`${buttonClass} ${props.baseClass ?? ""}`}
+            className={`${buttonClass} ${props.baseClass ?? ""} ${disabledStyle} ${disabledStyle}`}
         >
             {props.text}
         </button>

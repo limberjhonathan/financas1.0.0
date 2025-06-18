66d2417b0ceef3d50fdc9a9d69cf8c1d0f7177a0
Limber Jhonathan <limberjhonathan@gmail.com>
2025-06-17 23:52:44 -0300

    page login config

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,21 +1,29 @@
 
-import { TitleForm } from "@/src/components/form-title"
-import { LayoutBackForm } from "@/src/templates/layoutBack-form"
-import { generatePageMetadata } from "@/src/utils/page-title"
-import Link from "next/link"
-import LoginForm from "./login-form"
+import { TitleForm } from "@/src/components/form-title";
+import { LayoutBackForm } from "@/src/templates/layoutBack-form";
+import { generatePageMetadata } from "@/src/utils/page-title";
+import Link from "next/link";
+import LoginForm from "./login-form";
 
-export const metadata = generatePageMetadata("Login", "Login page")
+export const metadata = generatePageMetadata("Login", "Login page");
 
-export default function Login() {
+export default async function Login() {
 
-    return (
-        <LayoutBackForm>
-            <div>
-                <TitleForm>Login</TitleForm>
-                <LoginForm />
-                <p className="text-center p-2">Ainda não possui conta? <Link href={'/cadastrar'} className="text-[var(--form-bg)] font-bold cursor-pointer underline">Cadastre-se</Link></p>
-            </div>
-        </LayoutBackForm>
-    )
+  return (
+    <LayoutBackForm>
+      <div>
+        <TitleForm>Login</TitleForm>
+        <LoginForm />
+        <p className="text-center p-2">
+          Ainda não possui conta?{" "}
+          <Link
+            href={"/cadastrar"}
+            className="text-[var(--form-bg)] font-bold cursor-pointer underline"
+          >
+            Cadastre-se
+          </Link>
+        </p>
+      </div>
+    </LayoutBackForm>
+  );
 }

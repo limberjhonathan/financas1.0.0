c12c87c21914c639b8f36bc88c3d7fdf05418ee6
Limber Jhonathan <limberjhonathan@gmail.com>
2025-06-22 22:35:15 -0300

    config button for return

--- src/components/buttonAction.tsx
+++ src/components/buttonAction.tsx
@@ -1,23 +1,38 @@
+import { useEffect, useState } from "react";
 import { buttonClass } from "../utils/style/buttonStyle";
 
 interface ButtonActionProps {
-    text: string;
-    disabled?: boolean;
-    onClick?: () => void;
-    baseClass?: string;
-    type?: "button" | "submit" | "reset";
+  text: string;
+  disabled?: boolean;
+  onClick?: () => void;
+  baseClass?: string;
+  type?: "button" | "submit" | "reset";
 }
 
 export default function ButtonAction(props: ButtonActionProps) {
-    const disabledStyle = props.disabled ? "opacity-50 cursor-not-allowed" : ""
-    return (
-        <button
-            type={props.type}
-            disabled={props.disabled}
-            onClick={props.onClick}
-            className={`${buttonClass} ${props.baseClass ?? ""} ${disabledStyle} ${disabledStyle}`}
-        >
-            {props.text}
-        </button>
-    )
+  const [hasVerified, setHasVerified] = useState(false);
+
+  useEffect(() => {
+    const verified = localStorage.getItem("verificarAtivo");
+    setHasVerified(!!verified);
+  }, []);
+
+  const disabledStyle = props.disabled ? "opacity-50 cursor-not-allowed" : "";
+
+  return (
+    <button
+      type={props.type}
+      disabled={props.disabled}
+      onClick={() => {
+        if (hasVerified) {
+          window.location.reload();
+        } else {
+          props.onClick?.();
+        }
+      }}
+      className={`${buttonClass} ${props.baseClass ?? ""} ${disabledStyle}`}
+    >
+      {props.text}
+    </button>
+  );
 }

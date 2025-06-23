b43c6317e4a2bd72de382ce51e3357e94e33dea8
Limber Jhonathan <limberjhonathan@gmail.com>
2025-06-23 17:52:40 -0300

    input code verification

--- src/app/(private)/verificar/actions.ts
+++ src/app/(private)/verificar/actions.ts
@@ -1,7 +1,7 @@
 'use server';
 // import getEmailFromToken from "@/src/actions/getEmailToken";
 import { findUserByEmail } from "@/src/models/useModel";
-import { checkDateNow } from "@/src/services/userServices";
+import { checkDateNow, codeComparation } from "@/src/services/userServices";
 
 export async function checkCodeAction(email: string): Promise<void>{
   const user = await findUserByEmail(email);
@@ -14,5 +14,14 @@ export async function checkCodeAction(email: string): Promise<void>{
       user.confirmationCode.createdAt,
       user.confirmationCode.expiresAt
     );
+  }
+}
+
+export async function inputCodeVerification(_prevState: any, formData: FormData) {
+  const email = formData.get("email") as string;
+  const rawCode = formData.get("code") as string;
+  const code = rawCode.replace(/-/g, "");
+  if(code && email) {
+    await codeComparation(code, email);
   }
 }
